ff423e00857e0f9270564ac65ea834d492a40635
Ricardo Ziglia <ricardo.ziglia@herodigital.com>
2024-05-13 19:00:16 -0300

    Solution 18 fixes

--- force-app/main/default/aura/CTHealthHeader/CTHealthHeaderHelper.js
+++ force-app/main/default/aura/CTHealthHeader/CTHealthHeaderHelper.js
@@ -2,7 +2,7 @@
     fetchHealthyStatusCount: function (component) {
         const viewType = component.get("v.viewType");
 
-        let action = viewType === "person" ? component.get("c.getPersonHealthStatusCount") : component.get("c.getLocationHealthStatusCount");
+        let action = viewType === "person" ? component.get("c.getPersonHealthStatuscount") : component.get("c.getLocationHealthStatuscount");
 
         action.setCallback(this, function (response) {
             const state = response.getState();
