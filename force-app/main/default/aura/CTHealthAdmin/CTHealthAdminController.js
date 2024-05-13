853a4ce726baef8bd2de78056366a38817e9c0b7
Ricardo Ziglia <ricardo.ziglia@herodigital.com>
2024-05-13 14:21:56 -0300

    Solution 17 part 2

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -1,10 +1,6 @@
 ({
     tabsetSelectHandler : function(component, event, helper) {
         const selectedTabId = event.getParam("id");
-        if (selectedTabId === "person_view") {
-            component.set("v.headerTitle", "Person View");
-        } else {
-            component.set("v.headerTitle", "Location View");
-        }
+        component.set("v.viewType", selectedTabId);
     }
 })
--- force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
+++ force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
@@ -1,5 +1,17 @@
 ({
-    myAction : function(component, event, helper) {
-
+    viewTypeChange : function(component, event, helper) {
+        if (event.getParam("value") === "person") {
+            component.set("v.headerTitle", "Person View");
+        } else {
+            component.set("v.headerTitle", "Location View");
+        } 
+    },
+    createRecord : function (component, event, helper) {
+        const entityApiName = component.get("v.viewType") === "person" ? "Person__c" : "Location__c";
+        var createRecordEvent = $A.get("e.force:createRecord");
+        createRecordEvent.setParams({
+            "entityApiName": entityApiName
+        });
+        createRecordEvent.fire();
     }
 })
