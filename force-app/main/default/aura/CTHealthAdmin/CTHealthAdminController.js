dcd242b74ebc3bd1d8d4bc57e12bf54369b1ed8b
Ricardo Ziglia <ricardo.ziglia@herodigital.com>
2024-05-20 16:09:44 -0300

    Solution 19

--- force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
+++ force-app/main/default/aura/CTHealthAdmin/CTHealthAdminController.js
@@ -2,5 +2,8 @@
     tabsetSelectHandler : function(component, event, helper) {
         const selectedTabId = event.getParam("id");
         component.set("v.viewType", selectedTabId);
+
+        const healthHeaderComp = component.find("health-header");
+        healthHeaderComp.fetchCount();
     }
 })
--- force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
+++ force-app/main/default/aura/CTHealthHeader/CTHealthHeaderController.js
@@ -4,7 +4,9 @@
             component.set("v.headerTitle", "Person View");
         } else {
             component.set("v.headerTitle", "Location View");
-        } 
+        }
+        //Calling it from parent component via component method fetchCount
+        //helper.fetchHealthyStatusCount(component);
     },
     createRecord : function (component, event, helper) {
         const entityApiName = component.get("v.viewType") === "person" ? "Person__c" : "Location__c";
