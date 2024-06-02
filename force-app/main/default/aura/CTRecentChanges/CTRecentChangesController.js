09cadcc6cf9d3e66fed18eda654a94edf05d351a
Ricardo Ziglia <ricardo.ziglia@herodigital.com>
2024-06-02 01:31:38 -0300

    Solution 22 part 2

--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -1,10 +1,20 @@
 ({
     doInit: function (component, event, helper) {
         component.set("v.columns", helper.getColumns(component.get("v.viewType")));
-        helper.fetchResult(component);
+        helper.fetchRecentHealthChanges(component);
     },
 
-    onChangeViewType : function(component, event, helper) {
-        helper.fetchResult(component);
+    handleKeyUp: function (component, event, helper) {
+        var isEnterKey = event.keyCode === 13;
+        var queryTerm = component.find('enter-search').get('v.value');
+        if (!queryTerm) {
+            helper.searchHealthChanges(component, queryTerm);
+        } else if (isEnterKey) {
+            if (queryTerm) {
+                helper.searchHealthChanges(component, queryTerm);
+            } else {
+                helper.fetchRecentHealthChanges(component);
+            }
+        }
     }
 });
--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
@@ -1,15 +1,4 @@
 ({
-    fetchResult: function (component) {
-        let action = component.get("v.viewType") === "person" ? component.get("c.getRecentPersonHealthChanges") : component.get("c.getRecentLocationHealthChanges");
-        action.setCallback(this, function (response) {
-            const state = response.getState();
-            if (state === "SUCCESS") {
-                const resp = response.getReturnValue();
-                component.set("v.data", resp);
-            }
-        });
-        $A.enqueueAction(action);
-    },
     getColumns: function (viewType) {
         if (viewType === "person") {
             return [
@@ -30,5 +19,28 @@
                 { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
             ];
         }
+    },
+    fetchRecentHealthChanges: function (component) {
+        let action = component.get("v.viewType") === "person" ? component.get("c.getRecentPersonHealthChanges") : component.get("c.getRecentLocationHealthChanges");
+        action.setCallback(this, function (response) {
+            const state = response.getState();
+            if (state === "SUCCESS") {
+                const resp = response.getReturnValue();
+                component.set("v.data", resp);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    searchHealthChanges: function (component, queryTerm) {
+        let action = component.get("v.viewType") === "person" ? component.get("c.searchPeople") : component.get("c.searchLocation");
+        action.setParams({ searchTerm : queryTerm });
+        action.setCallback(this, function (response) {
+            const state = response.getState();
+            if (state === "SUCCESS") {
+                const resp = response.getReturnValue();
+                component.set("v.data", resp);
+            }
+        });
+        $A.enqueueAction(action);
     }
 });
