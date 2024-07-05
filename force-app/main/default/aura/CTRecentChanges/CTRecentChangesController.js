d4cebd6ff50ec7d165dbc90b5848711cdbed2452
Ricardo Ziglia <ricardo.ziglia@herodigital.com>
2024-07-05 15:52:42 -0300

    Solution 23 & 24

--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesController.js
@@ -5,7 +5,7 @@
     },
 
     handleKeyUp: function (component, event, helper) {
-        var isEnterKey = event.keyCode === 13;
+        const isEnterKey = event.keyCode === 13;
         var queryTerm = component.find('enter-search').get('v.value');
 
         if (isEnterKey) {
@@ -16,6 +16,22 @@
             }
         } else if (!queryTerm) {
             component.set("v.data", component.get("v.lastRecentHealthChangesResponse"));
+        }
+    },
+    handleRowAction: function (component, event, helper) {
+        const action = event.getParam("action");
+        const row = event.getParam("row");
+        const viewType = component.get("v.viewType");
+
+        switch (action.name) {
+            case "view_details":
+                const appEvent = viewType === "person" ? $A.get("e.c:CTPersonSelectEvent") : $A.get("e.c:CTLocationSelectEvent");
+                appEvent.setParams({
+                    recordId: row.Id,
+                    status: viewType === "person" ? row.Health_Status__c : row.Status__c
+                });
+                appEvent.fire();
+                break;
         }
     }
 });
--- force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
+++ force-app/main/default/aura/CTRecentChanges/CTRecentChangesHelper.js
@@ -6,8 +6,8 @@
                 { label: "Phone", fieldName: "Mobile__c", type: "phone" },
                 { label: "Token", fieldName: "Token__c", type: "text" },
                 { label: "Health Status", fieldName: "Health_Status__c", type: "text" },
-                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
-                
+                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" },
+                { label: "View", type: "button", initialWidth: 135, typeAttributes: { label: "View/Update", name: "view_details", title: "Click to View Person Details" } }
             ];
         } else {
             return [
@@ -16,7 +16,8 @@
                 { label: "Red Score", fieldName: "Red_Score__c", type: "number" },
                 { label: "Pincode", fieldName: "Pincode__c", type: "text" },
                 { label: "Address", fieldName: "Address__c", type: "text" },
-                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" }
+                { label: "Status Update Date", fieldName: "Status_Update_Date__c", type: "date" },
+                { label: "View", type: "button", initialWidth: 135, typeAttributes: { label: "View/Update", name: "view_details", title: "Click to View Location Details" } }
             ];
         }
     },
